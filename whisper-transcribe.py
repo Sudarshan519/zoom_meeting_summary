a89be333bad5098c1e3e9455791e31d13ef3a117
Sudarshan519 <sudarshanshrestha519@gmail.com>
2025-05-16 00:43:50 +0545

    mic with speaker test

--- whisper-transcribe.py
+++ whisper-transcribe.py
@@ -3,14 +3,16 @@ import torch
 import warnings
 
 warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
+import torch
+print(torch.cuda.is_available())
+print(torch.backends.mps.is_available())
 
-
-model = whisper.load_model("medium",device='cpu')
+model = whisper.load_model("medium")
 # if torch.backends.mps.is_available():
 #     model = model.to("mps")  # Move model to MPS GPU
 # else:
 # model = model.to("cpu")
-result = model.transcribe("temp_9pX_h3n54d8d98CNAAAH.wav",)
+result = model.transcribe("temp_s8fpu4Dhfg0xFSmIAAAB_1745936169.wav",)
 print(result["text"])
 # import torch
 # print(torch.backends.mps.is_available())
